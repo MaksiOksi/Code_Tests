612cbae7e1fc32d4636db36d70e4eb3deb0b045b
Maksym Dudko <mksmddk@gmail.com>
2023-02-04 14:41:00 +0200

    added modul setUp

--- test_survey.py
+++ test_survey.py
@@ -3,23 +3,25 @@ from survey import AnonymousSurvey
 
 class TestAnonymousSurvey(unittest.TestCase):
 
+    def setUp(self):
+        question = "What language do you first learn to speak ?"
+        self.my_survey = AnonymousSurvey(question)
+        self.responses = ['English','Spanish','Ukrainian']
 
     def test_store_single_response(self):
-        question = "What language do you first learn to speak ?"
-        my_survey = AnonymousSurvey(question)
-        my_survey.store_response('Python')
-        self.assertIn('Python', my_survey.responses)
+        self.my_survey.store_response(self.responses[0])
+        self.assertIn(self.responses[0], self.my_survey.responses)
 
     def test_store_few_responses(self):
         question = "What language do you first learn to speak ?"
         my_survey = AnonymousSurvey(question)
         responses = ['Python','Ruby',"C#"]
 
-        for response in responses:
-            my_survey.store_response(response)
+        for response in self.responses:
+            self.my_survey.store_response(response)
 
-        for response in responses:
-            self.assertIn(response, my_survey.responses)
+        for response in self.responses:
+            self.assertIn(response, self.my_survey.responses)
 
 if __name__ == '__main__':
     unittest.main
